0cb1e5ddf861e39d25d906dadd1f26b95c86c97e
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-23 01:09:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,22 +1,24 @@
 class Artist
-  attr_accessor :name 
+  attr_accessor :name
 
   @@all = []
 
   def initialize(name)
-    @name = name 
-    save 
-  end 
+    @name = name
+    save
+  end
 
-  def save 
-    @@all << self 
-  end 
+  def save
+    @@all << self
+  end
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
   def new_song(name, genre)
     song = Song.new(name, genre)
-    song.artist = self 
+    song.artist = self
   end
+
+end
