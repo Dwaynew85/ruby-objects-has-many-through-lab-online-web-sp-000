7899087fc56d6a363dcb8615e9d4df8213c07fbf
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-23 01:11:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,20 @@
-class Song 
-  attr_accessor :name, :genre 
+class Song
+  attr_accessor :name, :genre
 
   @@all = []
 
   def initialize(name, genre)
-    @name = name 
-    @gerne = genre 
-    save 
-  end 
+    @name = name
+    @gerne = genre
+    save
+  end
 
-  def save 
-    @@all << self 
-  end 
+  def save
+    @@all << self
+  end
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
 end
