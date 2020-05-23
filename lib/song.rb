f581660aec3b4d65e655ff84e034692ffa4ea335
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-23 01:30:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,12 +5,8 @@ class Song
 
   def initialize(name, genre)
     @name = name
-    @genre = genre
-    save
-  end
-
-  def save
-    @@all << self
+    @genre = genre 
+    @@all << self 
   end
 
   def self.all
