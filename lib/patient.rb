c6182b4a1c6f01a1c30a9368bf29f453c66aaaf3
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-23 01:57:05 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,25 @@
+class Patient 
+  attr_accessor :name
+
+  @@all = []
+
+  def initialze(name)
+    @name = name 
+    @@all << self 
+  end 
+
+  def new_appointment(date, doctor)
+    Appointment.new( date, self, doctor)
+  end 
+
+  def appointments 
+    Appointment.all.select {|appointment| appointment.name == self}
+  end 
+
+  def doctors 
+    self.appointments.map {|appointment| appointment.doctor}
+  end
+
+end
+
+  
