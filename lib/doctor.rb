707b209800e6fc891ed8203f16fd98f1dc325a0a
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-23 01:43:51 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,11 +1,11 @@
-class Doctor 
+class Doctor
   attr_accessor :name
 
   @@all = []
 
   def initialze(name)
-    @name = name 
-    @@all << self 
+    @name = name
+    @@all << self
   end
 
 end
